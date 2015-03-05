493e3f9f08da83ce11ca280d55aabe99c8c17ceb
Einar Löve <einar.love@gmail.com>
2015-03-05 15:15:51 +0100

    Move hours past midnight to the day before

--- src/scripts/stores/LineupStore.jsx
+++ src/scripts/stores/LineupStore.jsx
@@ -30,6 +30,7 @@ var LineupStore = Reflux.createStore({
   },
 
   sortHours(hours) {
+    console.log(hours)
     hours.sort((a, b) => {
       if(a.unixTimestamp < b.unixTimestamp) return -1
       if(a.unixTimestamp > b.unixTimestamp) return 1
