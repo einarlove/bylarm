d477ae945860d75a11cffab65bbf24e6f328df0d
Einar Löve <einar.love@gmail.com>
2015-03-05 17:50:47 +0100

    Add meta descriptions and open-graph info

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express()
 var config = require('./webpack.config')
 
 app.use(express.static('./build'))
+app.use(express.static('./static'))
 
 app.use(function(req, res) {
   res.sendFile(path.join(__dirname, '/src/index.html'))
